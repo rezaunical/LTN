02f0cf9554b02f3cf95cf25f1ba31e7fa270c55a
rezaunical <137186931+rezaunical@users.noreply.github.com>
2023-10-24 09:45:59 +0200

    Update binary_synthetic.py

--- binary_synthetic.py
+++ binary_synthetic.py
@@ -33,7 +33,7 @@ X, y = make_classification(n_samples=1000,
                            random_state=42)
 
 # Split into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.1, random_state=42)
 
 # Visualize the first two dimensions for a glimpse (though this won't capture the complexity)
 plt.figure(figsize=(6, 6))
